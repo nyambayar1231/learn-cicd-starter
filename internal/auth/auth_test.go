fce15f757aa3d1460517e53d593c38ffb412c413
nyambayar1231 <nyambayarlucky@gmail.com>
2024-11-26 15:48:15 +0800

    Add style job with go format

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -9,11 +9,11 @@ func TestGetApiKey(t *testing.T) {
 	headers := http.Header{}
 	headers.Set("Authorization", "ApiKey your-api-key")
 
-	apiKey, err :=  GetAPIKey(headers)
+	apiKey, err := GetAPIKey(headers)
 	if err != nil {
 		t.Errorf("Expected no error, got %v", err)
 	}
 	if apiKey != "your-api-key" {
-        t.Errorf("Expected 'your-api-key', got %v", apiKey)
-    }
+		t.Errorf("Expected 'your-api-key', got %v", apiKey)
+	}
 }

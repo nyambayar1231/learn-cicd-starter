3f74b2a1d61545eff5058fdf8af0fc649829f71a
nyambayar1231 <nyambayarlucky@gmail.com>
2024-11-26 17:18:44 +0800

    Formatting

--- json.go
+++ json.go
@@ -29,6 +29,6 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.WriteHeader(code)
 
 	if _, err := w.Write(dat); err != nil {
-		log.Printf("Error handling: %s", err)
+		log.Printf("Error handling2: %s", err)
 	}
 }
